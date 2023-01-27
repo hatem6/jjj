dca53de8350bb60d44acacfd54cb5def0edf339f
koussay-r <koussayr98@gmail.com>
2023-01-27 20:17:47 +0100

    update

--- Back-end-tinder/server.js
+++ Back-end-tinder/server.js
@@ -1,19 +1,23 @@
 import express from "express"
 import mongoose from "mongoose"
+import dotenv from "dotenv"
+import Cors from "cors"
+dotenv.config()
 //app config
 
 const app=express()
 const port=process.env.PORT || 8001
-
+const connectUrl=`mongodb+srv://admin:${process.env.PASSWORD}@cluster0.pishfig.mongodb.net/?retryWrites=true&w=majority`
 //MiddleWares
-
-
-
+app.use(express.json())
+app.use(Cors())
 //db conifg 
-
-
+mongoose.connect(connectUrl,{
+    useNewUrlParser:true
+})
+const db=mongoose.connection
 // Api endpoints
-app.get("/",(rea,res)=>{
+app.get("/",(req,res)=>{
     res.status(200).send("hello world")
 })
 
